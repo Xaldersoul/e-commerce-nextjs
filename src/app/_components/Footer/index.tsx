06177a332af2de8f8c220514218e0631c6990945
Xaldersoul <benjamin.g.guerra@hotmail.com>
2024-04-27 11:17:00 -0300

    login

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -3,8 +3,7 @@ import Link from 'next/link'
 
 import { Footer } from '../../../payload/payload-types'
 import { fetchFooter } from '../../_api/fetchGlobals'
-
-import classes from './index.module.scss'
+import FooterComponent from './FooterComponent'
 
 export async function Footer() {
   let footer: Footer | null = null
@@ -12,15 +11,14 @@ export async function Footer() {
   try {
     footer = await fetchFooter()
   } catch (error) {
-    console.log(error);
-    
+    console.log(error)
   }
 
   const navItems = footer?.navItems || []
 
   return (
     <>
-      
+      <FooterComponent footer={footer} />
     </>
   )
 }
